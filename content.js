52e6d8ad11816ee0856b6f81962e28b3314e8757
Yask Srivastava <yask123@gmail.com>
2016-02-20 22:19:47 +0530

    🍻🍻It's working!

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
 /**
  * Created by yasksrivastava on 20/02/16.
  */
-console.log('Hello World');
+$('.dropdown-menu ul').append('<li> <button id="link" type="button" class="dropdown-link fb_tweet">Share on Facebook</button> </li>');
+console.log('running');
+
+$('.fb_tweet').click(function(){
+    var content_node =  $(this).parents().eq(7).find('.tweet-timestamp').attr("href");
+    fb_share_url = 'https://twitter.com'+content_node;
+    console.log(fb_share_url);
+    window.open('http://www.facebook.com/sharer/sharer.php?u='+fb_share_url);
+});
